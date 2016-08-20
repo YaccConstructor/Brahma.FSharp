b0aef5aedd8e3c43cc61866e0bf0ab54278f8837
KochetovKirill <kirya.god@yandex.ru>
2016-08-20 20:58:45 +0300

    Tests for GPGPU removed

--- Source/HMM.Tests/HMM.Viterbi.Tests/GPGPU.fs
+++ Source/HMM.Tests/HMM.Viterbi.Tests/GPGPU.fs
@@ -4,15 +4,15 @@ open Viterbi_Parallel_GPGPU
 open NUnit.Framework
 open HMM.Viterbi.Tests.Src
 
-[<Test>]
+//[<Test>]
 let ``first`` () =
     HMM.Viterbi.Tests.Src.``first`` viterbi
 
-[<Test>]
+//[<Test>]
 let ``RF01315`` () =
     HMM.Viterbi.Tests.Src.hmmTestRF01315 viterbi
 
-[<Test>]
+//[<Test>]
 let ``RF02468`` () =
     HMM.Viterbi.Tests.Src.hmmTestRF02468 viterbi
 
