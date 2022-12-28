8c7e40b9d72daaa5bceda58b2612a60dcffa3542
gsv <rsdpisuy@gmail.com>
2022-12-28 23:37:22 +0300

    Bump version to 2.0.2
    ## [2.0.2] - 2022-12-28
    
    ### Added
    - .NET 7 is targeted
    
    ### Fixed
    - DUs are not blittable (.NET 7)

--- src/Brahma.FSharp.OpenCL.Shared/AssemblyInfo.fs
+++ src/Brahma.FSharp.OpenCL.Shared/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Brahma.FSharp.OpenCL.Shared")>]
 [<assembly: AssemblyProductAttribute("Brahma.FSharp")>]
-[<assembly: AssemblyVersionAttribute("2.0.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-08-05T00:00:00.0000000+03:00")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.0.1")>]
+[<assembly: AssemblyVersionAttribute("2.0.2")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-12-28T00:00:00.0000000+03:00")>]
+[<assembly: AssemblyFileVersionAttribute("2.0.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.0.2")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","b560b4d38e30cc06a46a85080a9c69b2ddd57082")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","8dd2acbb7a830be3b227c0091d18ebd432aa7ce1")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Brahma.FSharp.OpenCL.Shared"
     let [<Literal>] AssemblyProduct = "Brahma.FSharp"
-    let [<Literal>] AssemblyVersion = "2.0.1"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-08-05T00:00:00.0000000+03:00"
-    let [<Literal>] AssemblyFileVersion = "2.0.1"
-    let [<Literal>] AssemblyInformationalVersion = "2.0.1"
+    let [<Literal>] AssemblyVersion = "2.0.2"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-12-28T00:00:00.0000000+03:00"
+    let [<Literal>] AssemblyFileVersion = "2.0.2"
+    let [<Literal>] AssemblyInformationalVersion = "2.0.2"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "b560b4d38e30cc06a46a85080a9c69b2ddd57082"
+    let [<Literal>] AssemblyMetadata_GitHash = "8dd2acbb7a830be3b227c0091d18ebd432aa7ce1"
