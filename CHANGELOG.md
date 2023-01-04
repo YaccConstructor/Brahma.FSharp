bbc3c7c4846979bcf63f6259c1176d3886c600f1
gsv <rsdpisuy@gmail.com>
2023-01-04 12:28:21 +0300

    Bump version to 2.0.3
    ## [2.0.3] - 2023-01-04
    
    ### Fixed
    - All dependencies updated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.0.3] - 2023-01-04
+
+### Fixed
+- All dependencies updated
+
 ## [2.0.2] - 2022-12-28
 
 ### Added
@@ -264,7 +269,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - FSharp.Core form NuGet
 - .NET 4.5
 
-[Unreleased]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v2.0.2...HEAD
+[Unreleased]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v2.0.3...HEAD
+[2.0.3]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v2.0.2...v2.0.3
 [2.0.2]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v2.0.1...v2.0.2
 [2.0.1]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v2.0.0...v2.0.1
 [2.0.0]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v1.1.5...v2.0.0
--- src/Brahma.FSharp.OpenCL.Printer/AssemblyInfo.fs
+++ src/Brahma.FSharp.OpenCL.Printer/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Brahma.FSharp.OpenCL.Printer")>]
 [<assembly: AssemblyProductAttribute("Brahma.FSharp")>]
-[<assembly: AssemblyVersionAttribute("2.0.2")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-12-28T00:00:00.0000000+03:00")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.2")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.0.2")>]
+[<assembly: AssemblyVersionAttribute("2.0.3")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-01-04T00:00:00.0000000+03:00")>]
+[<assembly: AssemblyFileVersionAttribute("2.0.3")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.0.3")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","8dd2acbb7a830be3b227c0091d18ebd432aa7ce1")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","0d64bea9a5517d76be008f89514f7e4b3762fb19")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Brahma.FSharp.OpenCL.Printer"
     let [<Literal>] AssemblyProduct = "Brahma.FSharp"
-    let [<Literal>] AssemblyVersion = "2.0.2"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-12-28T00:00:00.0000000+03:00"
-    let [<Literal>] AssemblyFileVersion = "2.0.2"
-    let [<Literal>] AssemblyInformationalVersion = "2.0.2"
+    let [<Literal>] AssemblyVersion = "2.0.3"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-01-04T00:00:00.0000000+03:00"
+    let [<Literal>] AssemblyFileVersion = "2.0.3"
+    let [<Literal>] AssemblyInformationalVersion = "2.0.3"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "8dd2acbb7a830be3b227c0091d18ebd432aa7ce1"
+    let [<Literal>] AssemblyMetadata_GitHash = "0d64bea9a5517d76be008f89514f7e4b3762fb19"
