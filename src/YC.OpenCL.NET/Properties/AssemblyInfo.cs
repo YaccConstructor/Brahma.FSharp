3cacee63ace24060998d417180c2c669ce202a2c
gsv <rsdpisuy@gmail.com>
2023-01-04 12:28:21 +0300

    Bump version to 2.0.3
    ## [2.0.3] - 2023-01-04
    
    ### Fixed
    - All dependencies updated

--- src/YC.OpenCL.NET/Properties/AssemblyInfo.cs
+++ src/YC.OpenCL.NET/Properties/AssemblyInfo.cs
@@ -4,21 +4,21 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("YC.OpenCL.NET")]
 [assembly: AssemblyProduct("Brahma.FSharp")]
-[assembly: AssemblyVersion("2.0.2")]
-[assembly: AssemblyMetadata("ReleaseDate","2022-12-28T00:00:00.0000000+03:00")]
-[assembly: AssemblyFileVersion("2.0.2")]
-[assembly: AssemblyInformationalVersion("2.0.2")]
+[assembly: AssemblyVersion("2.0.3")]
+[assembly: AssemblyMetadata("ReleaseDate","2023-01-04T00:00:00.0000000+03:00")]
+[assembly: AssemblyFileVersion("2.0.3")]
+[assembly: AssemblyInformationalVersion("2.0.3")]
 [assembly: AssemblyMetadata("ReleaseChannel","release")]
-[assembly: AssemblyMetadata("GitHash","8dd2acbb7a830be3b227c0091d18ebd432aa7ce1")]
+[assembly: AssemblyMetadata("GitHash","0d64bea9a5517d76be008f89514f7e4b3762fb19")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "YC.OpenCL.NET";
         internal const System.String AssemblyProduct = "Brahma.FSharp";
-        internal const System.String AssemblyVersion = "2.0.2";
-        internal const System.String AssemblyMetadata_ReleaseDate = "2022-12-28T00:00:00.0000000+03:00";
-        internal const System.String AssemblyFileVersion = "2.0.2";
-        internal const System.String AssemblyInformationalVersion = "2.0.2";
+        internal const System.String AssemblyVersion = "2.0.3";
+        internal const System.String AssemblyMetadata_ReleaseDate = "2023-01-04T00:00:00.0000000+03:00";
+        internal const System.String AssemblyFileVersion = "2.0.3";
+        internal const System.String AssemblyInformationalVersion = "2.0.3";
         internal const System.String AssemblyMetadata_ReleaseChannel = "release";
-        internal const System.String AssemblyMetadata_GitHash = "8dd2acbb7a830be3b227c0091d18ebd432aa7ce1";
+        internal const System.String AssemblyMetadata_GitHash = "0d64bea9a5517d76be008f89514f7e4b3762fb19";
     }
 }
