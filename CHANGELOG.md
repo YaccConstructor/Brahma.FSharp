83f432d1ada8e1d437e524b34fd1255b5c41d73a
gsv <rsdpisuy@gmail.com>
2024-04-12 12:03:34 +0300

    Bump version to 3.0.0-alpha1.2
    ## [3.0.0-alpha1.2] - 2024-04-12
    
    ### Fixed
    - Raw float passing. (Issue #157)
    - Channel synchronization.
    
    ### Added
    - MailboxProcessor replaced with custom agent on System.Threading.Channels.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [3.0.0-alpha1.2] - 2024-04-12
+
+### Fixed
+- Raw float passing. (Issue #157)
+- Channel synchronization.
+
+### Added
+- MailboxProcessor replaced with custom agent on System.Threading.Channels.
+
 ## [3.0.0-alpha1.1] - 2024-04-09
 
 ### Added
@@ -287,7 +296,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - FSharp.Core form NuGet
 - .NET 4.5
 
-[Unreleased]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v3.0.0-alpha1.1...HEAD
+[Unreleased]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v3.0.0-alpha1.2...HEAD
+[3.0.0-alpha1.2]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v2.0.5...v3.0.0-alpha1.2
 [3.0.0-alpha1.1]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v2.0.5...v3.0.0-alpha1.1
 [2.0.5]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v2.0.4...v2.0.5
 [2.0.4]: https://github.com/YaccConstructor/Brahma.FSharp/compare/v2.0.3...v2.0.4
--- src/Brahma.FSharp.OpenCL.Shared/AssemblyInfo.fs
+++ src/Brahma.FSharp.OpenCL.Shared/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Brahma.FSharp.OpenCL.Shared")>]
 [<assembly: AssemblyProductAttribute("Brahma.FSharp")>]
-[<assembly: AssemblyVersionAttribute("2.0.3")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-01-04T00:00:00.0000000+03:00")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.0.3")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","0d64bea9a5517d76be008f89514f7e4b3762fb19")>]
+[<assembly: AssemblyVersionAttribute("3.0.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2024-04-09T00:00:00.0000000")>]
+[<assembly: AssemblyFileVersionAttribute("3.0.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.0.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","alpha1")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","4f6a4f8525c2a72218b70bfff07942674fac2f8a")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Brahma.FSharp.OpenCL.Shared"
     let [<Literal>] AssemblyProduct = "Brahma.FSharp"
-    let [<Literal>] AssemblyVersion = "2.0.3"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-01-04T00:00:00.0000000+03:00"
-    let [<Literal>] AssemblyFileVersion = "2.0.3"
-    let [<Literal>] AssemblyInformationalVersion = "2.0.3"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "0d64bea9a5517d76be008f89514f7e4b3762fb19"
+    let [<Literal>] AssemblyVersion = "3.0.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2024-04-09T00:00:00.0000000"
+    let [<Literal>] AssemblyFileVersion = "3.0.0"
+    let [<Literal>] AssemblyInformationalVersion = "3.0.0"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "alpha1"
+    let [<Literal>] AssemblyMetadata_GitHash = "4f6a4f8525c2a72218b70bfff07942674fac2f8a"
